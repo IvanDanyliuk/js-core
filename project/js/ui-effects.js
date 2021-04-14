8a93cc2c0dbf7469a59fa10de11912a86e939537
IvanDanyliuk <itsjustforserfing@gmail.com>
2021-04-14 16:50:08 +0300

    added tasks #1-7 from homework #7

---
+++ project/js/ui-effects.js
@@ -0,0 +1,16 @@
+//Accordion-effect for main-sections
+let acc = document.getElementsByClassName("main-section__heading");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        console.log(panel)
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
